3340e0578235146112b5078b32b3d0bfb2ae22c8
sousuke0422 <sousuke0422@users.noreply.github.com>
2020-12-20 08:32:37 +0000

    deploy: d086448c1c18148342774f417861431761e7f7f3

--- sw.js
+++ sw.js
@@ -19,7 +19,7 @@
 self.addEventListener('install', function () { return self.skipWaiting(); });
 self.addEventListener('active', function () { return self.clients.claim(); });
 
-toolbox.precache(['/','/2020/12/20/2020-12-20-yupix-diary/','/2020/12/20/2020-12-20-new-ayuki-blog/','/2020/12/05/2020-12-05-study-django/','/2020/12/05/2020-12-05-study-django1/','/2020/10/07/2020-10-07-handling-strings-in-python_2/']);
+toolbox.precache(['/','/2020/12/21/2020-12-21-misskey-advent-calendar-2020-ayuskey/','/2020/12/20/2020-12-20-yupix-diary/','/2020/12/20/2020-12-20-new-ayuki-blog/','/2020/12/05/2020-12-05-study-django/','/2020/12/05/2020-12-05-study-django1/']);
 toolbox.options = {"networkTimeoutSeconds":5};
 
 toolbox.router.any(/hm.baidu.com/, toolbox.networkOnly);
