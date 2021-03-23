9f63d3f3576030f7f67787cd450c9e38cea237be
yupix <yupix@users.noreply.github.com>
2021-03-23 11:11:32 +0000

    deploy: 622b016be9c1c66c013f39387d4ef4e83c5dca77

--- sw.js
+++ sw.js
@@ -19,7 +19,7 @@
 self.addEventListener('install', function () { return self.skipWaiting(); });
 self.addEventListener('active', function () { return self.clients.claim(); });
 
-toolbox.precache(['/','/2021/02/02/2021-02-02-issue-management-tool/','/2021/01/15/2021-01-15-study-discordpy/','/2020/12/21/2020-12-21-progress-intsl-py/','/2020/12/21/2020-12-21-misskey-advent-calendar-2020-ayuskey/','/2020/12/20/2020-12-20-yupix-diary/']);
+toolbox.precache(['/','/2021/03/23/2021-03-23-hexo-pwa-plugin-not-working/','/2021/02/02/2021-02-02-issue-management-tool/','/2021/01/15/2021-01-15-study-discordpy/','/2020/12/21/2020-12-21-progress-intsl-py/','/2020/12/21/2020-12-21-misskey-advent-calendar-2020-ayuskey/']);
 toolbox.options = {"networkTimeoutSeconds":5};
 
 toolbox.router.any("/hm.baidu.com/", toolbox.networkOnly);
